07c218f08312cae06d7a96542df3e0afbda9dd0b
Supun Akalanka <153161364+supunakalanka76@users.noreply.github.com>
2024-07-24 23:27:44 +0530

    updated-home

--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -7,22 +7,24 @@ function Home() {
     <div className='home'> 
       <div className='search-bar'>
       <span className="material-icons-outlined search">search</span>
-        <input className='search-Bar' id='search' placeholder='Search Here...' list='items' />
-          <datalist class='items'>
-            <option value='Electronics'></option>
-            <option value='Clothing'></option>
-            <option value='Groceries'></option>
-            <option value='Home'></option>
-            <option value='Books'></option>
-            <option value='Stationery'></option>
-            <option value='Miscellaneous'></option>
+        <input className='search-Bar' id='product' placeholder='Search Here...' list='items' />
+          <datalist id='items'>
+            <option value='Electronics'/>
+            <option value='Clothing'/>
+            <option value='Groceries'/>
+            <option value='Home'/>
+            <option value='Books'/>
+            <option value='Stationery'/>
+            <option value='Miscellaneous'/>
           </datalist>
 
           <div className='category'>
         <button className="btn-category">Category</button>
       </div>
+      {/* <div className='home-table'>
+        <p>Table</p>
+      </div> */}
       </div>
-      
     </div>
 
   )
