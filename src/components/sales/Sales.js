074cd9af9f5125a4da0730851c95fed44148dfd7
Supun Akalanka <153161364+supunakalanka76@users.noreply.github.com>
2024-07-29 22:00:42 +0530

    begin-sales

--- src/components/sales/Sales.js
+++ src/components/sales/Sales.js
@@ -1,9 +1,29 @@
 import React from 'react'
+import 'material-icons/iconfont/material-icons.css';
+import './Sales.css';
 
 function Sales() {
   return (
-    <div>
-      sales
+    <div className='sales'>
+      <div className='sales-search'>
+
+        <div className='search-Bar'>
+        <div className='row'>
+        <input type='text' id='input-box' placeholder='Search Here...' autoCapitalize='off'/>
+        </div>
+        </div>
+      <div className='sales-btn'>
+        <button className='btn-order'>Create Order</button>
+      </div>
+      </div>
+
+      <div className='sales-detail'>
+        <p>active Orders</p>
+        <p>Unfulfilled</p>
+        <p>Fulfilled</p>
+        <p>pending</p>
+      </div>
+      
     </div>
   )
 }
