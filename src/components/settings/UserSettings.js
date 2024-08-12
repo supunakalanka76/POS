5f23416dae997f3b40fb5febf24de51f6b6e141d
Supun Akalanka <153161364+supunakalanka76@users.noreply.github.com>
2024-08-12 21:17:18 +0530

    user-settings

---
+++ src/components/settings/UserSettings.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import Settings from './Settings'
+
+function UserSettings() {
+  return (
+    <div className='user-settings'>
+      <div className='user-header'>
+        <Settings/>
+      </div>
+      <div className='user-content'>
+        div
+      </div>
+    </div>
+  )
+}
+
+export default UserSettings
